2702dd6ea1bc3574ca43dd77c8a431eeeca0f4ae
Misha Slyusarev <mik3weider@gmail.com>
2017-04-27 18:45:34 +0200

    Add simple output for seeded data

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Container, Header, Dimmer, Loader, Divider } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.getRecipients = this.getRecipients.bind(this)
+  }
+  componentDidMount() {
+    this.getRecipients()
+  }
+  fetch(endpoint) {
+    return new Promise((resolve, reject) => {
+      window.fetch(endpoint)
+      .then(response => response.json())
+      .then(json => resolve(json))
+      .catch(error => reject(error))
+    })
+  }
+  getRecipients() {
+    this.fetch('v1/recipients')
+      .then(recipients => {
+        this.setState({recipients: recipients})
+        this.getRecipient(recipients[0].id)
+      })
+  }
+  getRecipient(id) {
+    this.fetch(`v1/recipients/${id}`)
+      .then(recipient => this.setState({recipient: recipient}))
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let {recipients, recipient} = this.state
+    return recipients
+    ? <Container text>
+        <Header as='h2' icon textAlign='center'>
+          <Header.Content>
+            List of recipients
+          </Header.Content>
+        </Header>
+      <Divider hidden />
+      {recipient &&
+        <Container>
+          <Header as='h2'>{recipient.name}</Header>
+          {recipient.surname && <p>{recipient.surname}</p>}
+          {recipient.address && <p>{recipient.address}</p>}
+        </Container>
+      }
+    </Container>
+    : <Container text>
+      <Dimmer active inverted>
+        <Loader content='Loading' />
+      </Dimmer>
+    </Container>
   }
 }
 
