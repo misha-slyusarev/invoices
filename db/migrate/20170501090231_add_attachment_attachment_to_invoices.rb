0bf6339dc3a818ecf88c9773be1cda31cc60ecb5
Misha Slyusarev <mik3weider@gmail.com>
2017-05-03 23:56:34 +0200

    Use modern migration format

--- db/migrate/20170501090231_add_attachment_attachment_to_invoices.rb
+++ db/migrate/20170501090231_add_attachment_attachment_to_invoices.rb
@@ -1,11 +1,5 @@
-class AddAttachmentAttachmentToInvoices < ActiveRecord::Migration
-  def self.up
-    change_table :invoices do |t|
-      t.attachment :attachment
-    end
-  end
-
-  def self.down
-    remove_attachment :invoices, :attachment
+class AddAttachmentAttachmentToInvoices < ActiveRecord::Migration[5.0]
+  def change
+    add_attachment :invoices, :attachment
   end
 end
