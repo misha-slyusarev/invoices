ed2bf67d433d2fc5675c4988174b8de2564a8ec2
Misha Slyusarev <mik3weider@gmail.com>
2017-05-03 20:56:57 +0200

    Add extra phone number validation

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,8 @@
 class Recipient < ApplicationRecord
   belongs_to :invoice, optional: true
-  validates_presence_of :name, :surname, :address, :phone
+  validates_presence_of :name, :surname, :address
+
+  validates :phone, presence: true,
+                    numericality: true,
+                    length: { maximum: 15 }
 end
