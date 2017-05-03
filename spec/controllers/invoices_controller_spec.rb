98a04621c8d2f8cb7449bf5c38e3caeb4eab2770
Misha Slyusarev <mik3weider@gmail.com>
2017-05-03 22:17:18 +0200

    Cover creating of Recipient and AdditionalFile with test

--- spec/controllers/invoices_controller_spec.rb
+++ spec/controllers/invoices_controller_spec.rb
@@ -5,21 +5,26 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
   let(:uploaded_invoice) {
     fixture_file_upload("#{Rails.root}/spec/fixtures/invoice.pdf", 'application/pdf')
   }
-
+  let(:uploaded_additional_file) {
+    fixture_file_upload("#{Rails.root}/spec/fixtures/additional_file.png", 'image/png')
+  }
   let(:valid_attributes) {
     {
       date: 'Wed May 03 2017 11:47:53 GMT+0200',
-      amount: '222',
+      amount: '777',
       attachment: uploaded_invoice,
       recipient_attributes: {
         name: 'John',
         surname: 'Doe',
         address: 'New York',
-        phone: '+1 123 456789'
-      }
+        phone: '+1123456789'
+      },
+      additional_files_attributes: [{
+        description: 'Additional file',
+        body: uploaded_additional_file
+      }]
     }
   }
-
   let(:invalid_attributes) {
     {
       date: nil,
@@ -30,20 +35,42 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
         surname: '',
         address: '',
         phone: ''
-      }
+      },
+      additional_files_attributes: [{
+        description: nil,
+        body: nil
+      }]
     }
   }
-
   let(:error_messages) {
-    "{\"recipient.name\":[\"can't be blank\"],\"recipient.surname\":[\"can't be blank\"],\"recipient.address\":[\"can't be blank\"],\"recipient.phone\":[\"can't be blank\"],\"attachment\":[\"can't be blank\"],\"amount\":[\"is not a number\"]}"
+    "{\"additional_files.body\":[\"can't be blank\"],\"recipient.name\":[\"can't be blank\"],"\
+    "\"recipient.surname\":[\"can't be blank\"],\"recipient.address\":[\"can't be blank\"],"\
+    "\"recipient.phone\":[\"can't be blank\",\"is not a number\"],\"attachment\":[\"can't be blank\"],"\
+    "\"date\":[\"can't be blank\"],\"amount\":[\"is not a number\"]}"
   }
 
   describe "POST #create" do
     context "with valid params" do
+      it 'responds with 200 OK' do
+        expect(response.status).to eq(200)
+      end
+
       it "creates a new Invoice" do
         expect {
           post :create, params: {invoice: valid_attributes}
         }.to change(Invoice, :count).by(1)
+      end
+
+      it "creates a new Recipient" do
+        expect {
+          post :create, params: {invoice: valid_attributes}
+        }.to change(Recipient, :count).by(1)
+      end
+
+      it "creates a new AdditionalFile" do
+        expect {
+          post :create, params: {invoice: valid_attributes}
+        }.to change(AdditionalFile, :count).by(1)
       end
     end
 
