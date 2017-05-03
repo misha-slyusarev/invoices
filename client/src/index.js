e112929dfa7c5607302a5f0067604b209b1e86b2
Misha Slyusarev <mik3weider@gmail.com>
2017-05-04 01:03:50 +0200

    Add missing NPM modules

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'semantic-ui-css/semantic.css'
-import 'react-dates/lib/css/_datepicker.css';
 import './styles/index.css';
 
 ReactDOM.render(
